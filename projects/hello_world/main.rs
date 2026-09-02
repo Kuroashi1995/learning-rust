9473a01844aee92d079a527ba70d12016653b6a1
Andrew Halley <kuroashi.prog@gmail.com>
2026-09-02 16:58:30 -0300

    rs file structure learning

--- projects/hello_world/main.rs
+++ projects/hello_world/main.rs
@@ -1,3 +1,9 @@
+// It uses the classic main entry function
+// fn is the function declaration keyword
 fn main() {
+    // Here is the first thing to be noticed, the ! at the function call, it means it is not a
+    // function call at all, it is indeed a macro call. Will learn more about that later.
     println!("Hello, world!");
 }
+
+// Rust macros are a way to write code that generates code to extend Rust syntax (WTF does this mean)
