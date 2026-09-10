5be3ecf79b277e56eb5529b7f4f93370591a24b1
Andrew Halley <kuroashi.prog@gmail.com>
2026-09-10 13:05:04 -0300

    finish structs

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,11 +1,26 @@
+struct Rectangle {
+    width: u32,
+    heigth: u32,
+}
 fn main() {
-    let width = 5;
-    let height = 10;
+
+    let rectangle = Rectangle {
+        width: 5,
+        heigth: 10,
+    };
+
+    //This is the not using struct example
+    // let width = 5;
+    // let height = 10;
 
 
-    println!("The area of the rectangle is {}", area(width, height));
+    println!("The area of the rectangle is {}", area_struct(&rectangle));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.heigth
 }
