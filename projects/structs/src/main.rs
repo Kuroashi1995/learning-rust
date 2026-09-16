a2b44d73f5bfc64f07c2338eb852f94c1c999265
Andrew Halley <kuroashi.prog@gmail.com>
2026-09-16 08:25:59 -0300

    Structs & methods

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,26 +1,47 @@
 struct Rectangle {
     width: u32,
-    heigth: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn constructor(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height
+        }
+    }
 }
 fn main() {
 
-    let rectangle = Rectangle {
-        width: 5,
-        heigth: 10,
-    };
+    let r1 = Rectangle::constructor(5, 10);
+
+    let r2 = Rectangle::constructor(10, 15);
+
+    let r3 = Rectangle::constructor(2, 4);
+
+
+    println!("r1 has an area of {}", r1.area());
+    println!("Can r1 hold r2?: {}", r1.can_hold(&r2));
+    println!("Can r1 hold r3?: {}", r1.can_hold(&r3));
 
     //This is the not using struct example
     // let width = 5;
     // let height = 10;
 
-
-    println!("The area of the rectangle is {}", area_struct(&rectangle));
 }
 
 // fn area(width: u32, height: u32) -> u32 {
 //     width * height
 // }
 
-fn area_struct(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.heigth
-}
+// fn area_struct(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.heigth
+// }
